42cc3ec4ca4273446e31f95f742d2077daeb6783
ducthanh512 <ducthanh512@gmail.com>
2018-07-12 12:10:16 +1000

    Change to Amazone DB

--- connection.js
+++ connection.js
@@ -10,10 +10,10 @@ function Connection() {
       // user: 'root',
       // password: '123456',
       // database: 'MusicDB',
-      host: 'us-cdbr-iron-east-04.cleardb.net',
-      user: 'b21da7aaabf93c',
-      password: 'c38472cf',
-      database: 'heroku_1d252267e6e485f'
+      host: 'musicappdb.caetv9pd8to1.us-east-1.rds.amazonaws.com',
+      user: 'root',
+      password: '12345678',
+      database: 'MusicAppDB'
     });
   };
   
